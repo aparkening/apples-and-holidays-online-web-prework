e46c758f2b13c2d83c9b00137ead3111fed6cc9c
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-29 14:06:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,6 +33,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash
 end
 
+add_supply_to_winter_holidays(holiday_hash, supply)
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
