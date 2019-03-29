9fe459970263fb527c0516cf087baaa07f4fd367
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-29 13:51:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,8 +29,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
-  holiday_hash[:winter].collect do |[:fourth_of_july][1]
-
+  holiday_hash[:winter].collect do |holiday, gear| 
+    
+    [:fourth_of_july][1]
+  end
 
 end
 
