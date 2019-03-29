c083358a51cb4b3407f34baa18da5e1d41f21381
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-29 15:51:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,21 @@ def all_holidays_with_bbq(holiday_hash)
   key_array
 end
 
-
+holiday_hash = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 all_holidays_with_bbq(holiday_hash)
 
 
