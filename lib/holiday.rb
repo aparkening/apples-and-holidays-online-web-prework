5d8ca96e2db2a45948fe7d45e0184484207b59bd
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-29 13:51:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
   holiday_hash[:winter].collect do |holiday, gear| 
-    
+    holiday << supply 
     [:fourth_of_july][1]
   end
 
