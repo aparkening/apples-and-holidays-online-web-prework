30b3ab61dfb6b54e22b57edbd7846aeb1f7f25a3
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-29 15:19:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
       
     holiday.each do |holiday_name, supply|
       holiday_name = holiday_name.to_s.capitalize
-      holiday_string = "#{holiday_name}:"
+      holiday_string = "#{holiday_name}: "
       supply_string = supply.join(", ")
       holiday_string << supply_string
       binding.pry
